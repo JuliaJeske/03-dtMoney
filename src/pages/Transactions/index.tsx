974e8040c92c82f5f52b6013d7a9c09714a09d23
juliajeske <julia.jeske@wehandle.com.br>
2023-02-12 15:15:32 -0300

    create api in react project

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,36 @@
+import { useEffect, useState } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { SearchForm } from "./components/SearchForm";
 import { PriceHighLight, TransactionsContainer, TransactionsTable } from "./styles";
 
+interface Transaction {
+  id: number;
+  description: string;
+  type: 'income' | 'outcome';
+  price: number;
+  category: string;
+  createdAt: string;
+
+}
+
+
 export function Transactions(){
+
+  //armazenar informações dentro do react para serem consumidas pela interface
+  const [transactions, setTransactions] = useState<Transaction[]>([])
+
+  async function loadTransactions(){
+    const response = await fetch('http://localhost:3000/transactions')
+    const data = await response.json();
+
+   setTransactions(data);
+  }
+
+  useEffect(()=> {
+    loadTransactions();
+  }, [])
+
   return (
     <div>
       <Header/>
@@ -12,20 +39,18 @@ export function Transactions(){
         <SearchForm />
       <TransactionsTable>
         <tbody>
-          <tr>
-            <td width='50%'>Desenvolvimento de site</td>
-            <td>
-              <PriceHighLight variant="income">R$ 12.000,00</PriceHighLight>
-            </td>
-            <td>Venda</td>
-            <td>13/04/2022</td>
-          </tr>
-          <tr>
-            <td width='50%'>Hambúrguer</td>
-            <td><PriceHighLight variant="outcome">R$ 12.000,00</PriceHighLight></td>
-            <td>Venda</td>
-            <td>13/04/2022</td>
-          </tr>
+          {transactions.map(transaction => {
+            return (
+              <tr key={transaction.id}>
+              <td width='50%'>{transaction.description}</td>
+              <td>
+                <PriceHighLight variant={transaction.type}>{transaction.price}</PriceHighLight>
+              </td>
+              <td>{transaction.category}</td>
+              <td>{transaction.createdAt}</td>
+            </tr>
+            )
+          })}
         </tbody>
       </TransactionsTable>
       </TransactionsContainer>
