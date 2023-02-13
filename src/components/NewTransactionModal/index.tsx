a870ad0de08bd8f0b21127a9731da0bd09e4fe3a
juliajeske <julia.jeske@wehandle.com.br>
2023-02-13 17:32:06 -0300

    configuration of axios

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -3,13 +3,13 @@ import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react';
 import { CloseButton, Content, Overlay, TransactionType, TransactionTypeButton } from './styles';
 import * as z from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { useForm } from 'react-hook-form';
+import { Controller, useForm } from 'react-hook-form';
 
 const newTransactionFormSchema = z.object({
   description: z.string(),
   price: z.number(),
   category: z.string(),
- // type: z.enum(['income', 'outcome']),
+ type: z.enum(['income', 'outcome']),
 })
 
 type NewTransactionFormInputs = z.infer<typeof newTransactionFormSchema>
@@ -17,6 +17,7 @@ type NewTransactionFormInputs = z.infer<typeof newTransactionFormSchema>
 export function NewTransactionModal(){
 
  const {
+  control,
   register,
   handleSubmit,
   formState: {isSubmitting}
@@ -56,17 +57,28 @@ export function NewTransactionModal(){
         {...register('category')}
         />
 
-        <TransactionType>
-          <TransactionTypeButton variant='income' value='income'>
-            <ArrowCircleUp size={24}/>
-            Entrada
-          </TransactionTypeButton>
-          <TransactionTypeButton variant='outcome' value='outcome'>
-            Saida
-            <ArrowCircleDown size={24}/>
-          </TransactionTypeButton>
-        </TransactionType>
+    
 
+        <Controller
+        control={control}
+        name='type'
+        render={({field})=> {
+          return (
+            <TransactionType onValueChange={field.onChange} value={field.value}>
+            <TransactionTypeButton variant='income' value='income'>
+              <ArrowCircleUp size={24}/>
+              Entrada
+            </TransactionTypeButton>
+            <TransactionTypeButton variant='outcome' value='outcome'>
+              Saida
+              <ArrowCircleDown size={24}/>
+            </TransactionTypeButton>
+          </TransactionType>
+          )
+        }}
+        />
+
+       
         <button type='submit' disabled={isSubmitting}>Cadastrar</button>
       </form>
      
--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -3,6 +3,8 @@ import { useForm } from "react-hook-form";
 import { SearchFormContainer } from "./styles";
 import * as z from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
+import { useContext } from "react";
+import { TransactionsContext } from "../../../../context/TransactionsContext";
 
 const searchFormSchema = z.object({
   query: z.string(),
@@ -11,6 +13,9 @@ const searchFormSchema = z.object({
 type SearchFormInputs = z.infer<typeof searchFormSchema>
 
 export function SearchForm(){
+
+
+  const { fetchTransactions } = useContext(TransactionsContext)
 
   const { 
       register,
@@ -21,8 +26,8 @@ export function SearchForm(){
   })
 
   //funções de acoes feitas pelo usuário - handle
-  function handleSearchTransactions(data: SearchFormInputs) {
-    
+  async function handleSearchTransactions(data: SearchFormInputs) {
+    await fetchTransactions(data.query)
   }
 
   return(
